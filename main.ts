12db3066e4e67284af19f3f6ecdb73e2ce499a00
Jakub <jakubhanula0@gmail.com>
2025-11-28 15:40:29 +0100

    remove bun.lock from GH; create basic functions

--- main.ts
+++ main.ts
@@ -1,1 +1,128 @@
-console.log("Hello via Bun!")
+import fs from "fs"
+import path from "path"
+import child_process from "child_process"
+
+function FileSync(path: string) {
+  return new CFileSync(path)
+}
+
+class CFileSync {
+  path
+
+  constructor(path: string) {
+    this.path = path
+  }
+
+  text() {
+    return String(fs.readFileSync(this.path))
+  }
+
+  json() {
+    return JSON.parse(this.text())
+  }
+
+  write(text: string) {
+    const arr = this.path.split(`/`)
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (!fs.existsSync(arr[i])) fs.mkdirSync(arr[i])
+    }
+    fs.writeFileSync(this.path, text)
+  }
+
+  exists() {
+    return fs.existsSync(this.path)
+  }
+
+  delete() {
+    fs.unlinkSync(this.path)
+  }
+}
+
+const runtimes = [`bun`, `node`, `deno`, `npm`, `yarn`, `pnpm`]
+let validRuntime: string | null = null
+
+function getValidRuntime(runtimes: string[]) {
+  for (const rt of runtimes) {
+    try {
+      const a = child_process.spawnSync(rt, {
+        timeout: 5,
+      })
+
+      if (a.status === 0) return rt
+    } catch {}
+  }
+
+  Err(`No valid runtime, runtimes:`, runtimes)
+}
+
+export function spawnSync({ runtime, cmd, ...options }: spawnSyncOptions) {
+  let firstArg = ``
+  if (runtime) {
+    if (typeof runtime !== `boolean`) validRuntime = getValidRuntime(runtime)
+    else if (!validRuntime) validRuntime = getValidRuntime(runtimes)
+    firstArg = validRuntime
+  } else {
+    firstArg = cmd[0]
+    cmd = cmd.slice(1)
+  }
+
+  if (cmd.length > 0) child_process.spawnSync(firstArg, cmd, options)
+  else child_process.spawnSync(firstArg, options)
+}
+
+export function Err(...args: any[]): never {
+  console.error(...args)
+  process.exit()
+}
+
+export { fs, path, child_process, FileSync }
+
+// Types
+
+type spawnSyncOptions = {
+  cmd: string[]
+  cwd?: string
+  runtime?: true | string[]
+  stdio?: child_process.StdioOptions
+  timeout?: number
+  killSignal?: number | Signals | undefined
+}
+
+type Signals =
+  | "SIGABRT"
+  | "SIGALRM"
+  | "SIGBUS"
+  | "SIGCHLD"
+  | "SIGCONT"
+  | "SIGFPE"
+  | "SIGHUP"
+  | "SIGILL"
+  | "SIGINT"
+  | "SIGIO"
+  | "SIGIOT"
+  | "SIGKILL"
+  | "SIGPIPE"
+  | "SIGPOLL"
+  | "SIGPROF"
+  | "SIGPWR"
+  | "SIGQUIT"
+  | "SIGSEGV"
+  | "SIGSTKFLT"
+  | "SIGSTOP"
+  | "SIGSYS"
+  | "SIGTERM"
+  | "SIGTRAP"
+  | "SIGTSTP"
+  | "SIGTTIN"
+  | "SIGTTOU"
+  | "SIGUNUSED"
+  | "SIGURG"
+  | "SIGUSR1"
+  | "SIGUSR2"
+  | "SIGVTALRM"
+  | "SIGWINCH"
+  | "SIGXCPU"
+  | "SIGXFSZ"
+  | "SIGBREAK"
+  | "SIGLOST"
+  | "SIGINFO"
